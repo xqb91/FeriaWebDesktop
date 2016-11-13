606f1aa18a860579c29f9362067f9920beca63e6
xqb91 <victor.araya92@gmail.com>
2016-11-12 21:02:44 -0300

    Feria Web 1.0822
    Avances:
    ComprasClientes
    UltimaCanasta

--- FeriaWebApp/src/cl/alevicmar/menuClientes/UltimaCanasta.java
+++ FeriaWebApp/src/cl/alevicmar/menuClientes/UltimaCanasta.java
@@ -12,19 +12,18 @@ import cl.alevicmar.services.metodopago.WebServiceMetodopago;
 import cl.alevicmar.services.ordenventa.WebServiceOrdendeventa;
 import cl.alevicmar.services.producto.WebServiceProducto;
 import cl.alevicmar.tools.HR;
-import cl.alevicmar.tools.HRut;
 import java.awt.Component;
-import java.util.Locale;
 
 /**
  *
- * @author cetecom
+ * @author Janno
  */
-public class ComprasCliente extends javax.swing.JFrame {
+public class UltimaCanasta extends javax.swing.JFrame {
 
     /**
-     * Creates new form ComprasCliente
+     * Creates new form UltimaCanasta
      */
+    
     WebServiceCliente srvCliente = null;
     WebServiceEgreso srvEgreso = null;
     WebServiceMetodopago srvMetodo = null;
@@ -32,7 +31,7 @@ public class ComprasCliente extends javax.swing.JFrame {
     WebServiceProducto srvProducto = null;
     Component componente = null;
     
-    public ComprasCliente(WebServiceCliente srvCliente, WebServiceEgreso srvEgreso, WebServiceMetodopago srvMetodo, WebServiceOrdendeventa srvOrdenDeVenta, WebServiceProducto srvProducto, Component componente)
+    public UltimaCanasta(WebServiceCliente srvCliente, WebServiceEgreso srvEgreso, WebServiceMetodopago srvMetodo, WebServiceOrdendeventa srvOrdenDeVenta, WebServiceProducto srvProducto, Component componente)
     {
         initComponents();
         this.srvCliente = srvCliente;
@@ -47,7 +46,7 @@ public class ComprasCliente extends javax.swing.JFrame {
         }
     }
     
-    public ComprasCliente() {
+    public UltimaCanasta() {
         initComponents();
         this.srvCliente = new WebServiceCliente();
         this.srvEgreso = new WebServiceEgreso();
@@ -56,7 +55,6 @@ public class ComprasCliente extends javax.swing.JFrame {
         this.srvProducto = new WebServiceProducto();
         this.setLocationRelativeTo(componente);
     }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -70,28 +68,17 @@ public class ComprasCliente extends javax.swing.JFrame {
         txtRun = new javax.swing.JTextField();
         btnBuscar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        tableCompraCliente = new javax.swing.JTable();
-        jLabel1 = new javax.swing.JLabel();
+        tblUltimaCanasta = new javax.swing.JTable();
+        lblTotal = new javax.swing.JLabel();
+        lblFecha = new javax.swing.JLabel();
+        lblMetodoDePago = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("Feria Web Desktop Client  ::  Compras del Cliente");
+        setTitle("Feria Web Desktop Client  ::  Ultima Canasta");
 
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Compras de Cliente"));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Ultima Canasta"));
 
-        txtRun.addFocusListener(new java.awt.event.FocusAdapter() {
-            public void focusLost(java.awt.event.FocusEvent evt) {
-                txtRunFocusLost(evt);
-            }
-        });
-        txtRun.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtRunActionPerformed(evt);
-            }
-        });
         txtRun.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                txtRunKeyPressed(evt);
-            }
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 txtRunKeyTyped(evt);
             }
@@ -105,17 +92,21 @@ public class ComprasCliente extends javax.swing.JFrame {
             }
         });
 
-        tableCompraCliente.setModel(new javax.swing.table.DefaultTableModel(
+        tblUltimaCanasta.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "Fecha", "Producto", "Cantidad", "Metodo de Pago", "SubTotal", "Total"
+                "Producto", "Cantidad", "SubTotal"
             }
         ));
-        jScrollPane1.setViewportView(tableCompraCliente);
+        jScrollPane1.setViewportView(tblUltimaCanasta);
 
-        jLabel1.setText("Run:");
+        lblTotal.setText("Total:");
+
+        lblFecha.setText("Fecha: ");
+
+        lblMetodoDePago.setText("Metodo de Pago:");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -124,15 +115,23 @@ public class ComprasCliente extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 566, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(18, 18, 18)
-                        .addComponent(txtRun, javax.swing.GroupLayout.PREFERRED_SIZE, 205, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(btnBuscar)
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addContainerGap())
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(txtRun, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(btnBuscar)
+                                .addGap(0, 201, Short.MAX_VALUE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(lblFecha)
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addContainerGap())
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblMetodoDePago)
+                            .addComponent(lblTotal))
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -140,10 +139,15 @@ public class ComprasCliente extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtRun, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnBuscar)
-                    .addComponent(jLabel1))
+                    .addComponent(btnBuscar))
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(lblFecha)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblMetodoDePago)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblTotal)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -169,41 +173,14 @@ public class ComprasCliente extends javax.swing.JFrame {
 
     private void txtRunKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtRunKeyTyped
         // TODO add your handling code here:
+        HR.esVacio(txtRun);
         HR.ingresaCaracteresRut(evt);
         HR.largoMaximo(txtRun, 12, evt);
     }//GEN-LAST:event_txtRunKeyTyped
 
     private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
         // TODO add your handling code here:
-        
     }//GEN-LAST:event_btnBuscarActionPerformed
-
-    private void txtRunFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtRunFocusLost
-        // TODO add your handling code here:
-        if(!HR.contenido(txtRun).isEmpty())
-        {
-            HR.insertarTexto(txtRun, HRut.formatear(HR.contenido(txtRun)));
-            if(!HRut.validar(HR.contenido(txtRun)))
-            {
-                HR.mostrarError("El Run ingresado no es válido");
-                HR.seleccionarTodo(txtRun);
-                HR.focus(txtRun);
-            }
-        }
-    }//GEN-LAST:event_txtRunFocusLost
-
-    private void txtRunKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtRunKeyPressed
-        // TODO add your handling code here:
-        /*if(HR.haPresionadoEnter(evt))
-        {
-            btnBuscarActionPerformed(evt);
-                    
-        }*/
-    }//GEN-LAST:event_txtRunKeyPressed
-
-    private void txtRunActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtRunActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtRunActionPerformed
 
     /**
      * @param args the command line arguments
@@ -222,30 +199,32 @@ public class ComprasCliente extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ComprasCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UltimaCanasta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ComprasCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UltimaCanasta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ComprasCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UltimaCanasta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ComprasCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UltimaCanasta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ComprasCliente().setVisible(true);
+                new UltimaCanasta().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBuscar;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable tableCompraCliente;
+    private javax.swing.JLabel lblFecha;
+    private javax.swing.JLabel lblMetodoDePago;
+    private javax.swing.JLabel lblTotal;
+    private javax.swing.JTable tblUltimaCanasta;
     private javax.swing.JTextField txtRun;
     // End of variables declaration//GEN-END:variables
 }
